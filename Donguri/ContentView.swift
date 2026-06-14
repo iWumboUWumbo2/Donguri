2ebfb9d0117719377c1334f1673c02cbbcdf0d76
Arun Raman (iWumboUWumbo2) <arunraman3223@gmail.com>
2026-06-14 14:26:17 -0400

    Initial Commit

---
+++ Donguri/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Donguri
+//
+//  Created by John Connery on 6/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Donguri/DonguriApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonguriApp.swift
+//  Donguri
+//
+//  Created by John Connery on 6/14/26.
+//
+
+import SwiftUI
+
+@main
+struct DonguriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
